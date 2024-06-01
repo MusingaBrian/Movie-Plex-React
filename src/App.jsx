a92aaf20a16274e857bda4f33e64f73fe8117981
MusingaBrian <musingabrians@gmail.com>
2024-06-01 16:20:15 +0300

    Add the Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className='glass max-h-full max-h-full' data-theme='cyberpunk'>
       <Navbar/>
+      <section className='h-4/5'>
+
+      </section>
       <Footer/>
     </div>
   )
