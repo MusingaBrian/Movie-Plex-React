c4695707af275d486b3d3bb8bd77137d02e12556
MusingaBrian <musingabrians@gmail.com>
2024-06-01 17:00:19 +0300

    Add the movie section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
+import Movie from './components/Movie'
 
 function App() {
   return (
     <div className='glass max-h-full max-h-full' data-theme='cyberpunk'>
-      <Navbar/>
-      <section className='glass'>
-        <Hero />
+      <Navbar />
+      <section className='flex flex-row justify-between gap-3 flex-wrap p-5'>
+        <Movie />
+        <Movie />
+        <Movie />
+        <Movie />
+        <Movie />
+        <Movie />
       </section>
+      <Hero />
       <Footer/>
     </div>
   )
