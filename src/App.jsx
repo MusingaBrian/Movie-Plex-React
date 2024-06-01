f48c00ffd1f4e34786f64a781a033bbffd5cfd62
MusingaBrian <musingabrians@gmail.com>
2024-06-01 16:10:03 +0300

    Initial Setup and Implement Navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className='glass max-h-full max-h-full' data-theme='cyberpunk'>
+      <Navbar/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
